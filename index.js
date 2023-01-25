be87b7d4a7b32d3eda67341667e6b6c10f9b1d36
nanaawesome <34039612+nanaawesome@users.noreply.github.com>
2023-01-25 21:07:18 +0000

    d

--- index.js
+++ index.js
@@ -12,14 +12,13 @@ form.addEventListener("submit",function(event){
         var x = day+((13*(month+1))/5)+k+(k/4)+(j/4)-(2*j);
         var h = Math.ceil(x%7);
         var d = ((h+5)%7)+1;
-        return x;
+        return d;
     }
     
     let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     
     var dayText=days[dayofWeek(day,month,year)];
-    console.log("Month: ")
-    console.log(typeof month);
+    console.log(dayText);
     document.getElementById('textofDay').innerHTML=dayText;
 
 })
