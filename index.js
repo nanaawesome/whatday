2b50a87654cdedf2e008748a0c38dc0660561bfc
nanaawesome <34039612+nanaawesome@users.noreply.github.com>
2023-01-25 16:26:42 +0000

    add vars

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-let day = document.getElementById('day').value;
-let month = document.getElementById('month').value;
-let year = document.getElementById('year').value;
+var day = document.getElementById('day').value;
+var month = document.getElementById('month').value;
+var year = document.getElementById('year').value;
 
 function dayofWeek(day,month,year){
-    k = year%100;
-    J = Math.floor(year/100);
-    h = Math.ceil((day + (13*(month+1)/5)+k+(k/4)+(J/4)-2*J)%7);
-    return d = ((h+5)%7)+1;
+    var k = year%100;
+    var J = Math.floor(year/100);
+    var h = Math.ceil((day + (13*(month+1)/5)+k+(k/4)+(J/4)-2*J)%7);
+    var d = ((h+5)%7)+1;
+    return d;
 }
 
 let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
