cdc169408e594c1610e568eff4b9645cafb90abe
nanaawesome <34039612+nanaawesome@users.noreply.github.com>
2023-01-25 16:38:37 +0000

    add event listener

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-var day = document.getElementById('day').value;
-var month = document.getElementById('month').value;
-var year = document.getElementById('year').value;
+let form = document.getElementById('date');
 
-function dayofWeek(day,month,year){
-    var k = year%100;
-    var J = Math.floor(year/100);
-    var h = Math.ceil((day + (13*(month+1)/5)+k+(k/4)+(J/4)-2*J)%7);
-    var d = ((h+5)%7)+1;
-    return d;
-}
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+    let day = document.getElementById('day').value;
+    let month = document.getElementById('month').value;
+    let year = document.getElementById('year').value;
 
-let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    function dayofWeek(day,month,year){
+        var k = year%100;
+        var J = Math.floor(year/100);
+        var h = Math.ceil((day + (13*(month+1)/5)+k+(k/4)+(J/4)-2*J)%7);
+        var d = ((h+5)%7)+1;
+        return d;
+    }
+    
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    
+    var dayText=days[dayofWeek];
+    console.log(dayText);
+    document.getElementById('textofDay').innerHTML=dayText;
 
-var dayText=days[dayofWeek];
-console.log(dayText);
-document.getElementById('textofDay').innerHTML=dayText;
+})
+
+
