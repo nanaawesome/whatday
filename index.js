1b2e689172ff2de471b9da696003bad257fccc55
nanaawesome <34039612+nanaawesome@users.noreply.github.com>
2023-01-25 18:50:32 +0000

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ form.addEventListener("submit",function(event){
         var J = Math.floor(year/100);
         var h = Math.ceil((day + ((13*(month+1))/5)+k+(k/4)+(J/4)-2*J)%7);
         var d = ((h+5)%7)+1;
-        return h;
+        return d;
     }
     
     let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
